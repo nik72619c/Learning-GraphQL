87096debf470003cb32ac2bfeac56142e2c1b612
nik72619c <nikhilsharmarockstar21@gmail.com>
2019-03-20 04:07:17 +0530

    express-server

--- using-express-server/app.js
+++ using-express-server/app.js
@@ -1,16 +1,20 @@
 const express=require('express');
 const graphqlHTTP=require('express-graphql');
 const {buildSchema}=require('graphql');
+const fakeData=require('./fakeDB');
 
 const schema=buildSchema(`
 type Query{
-    name: String,
-    age: Int
+    name: [String],
+    age: Int,
+    city: String
 }
 `);   //making our query
 var root={
     name: ()=>{
-
+            let names=fakeData.user.filter(element=>element.name);
+            return fakeData.user;
+           
     },
     age: ()=>{
 
---
+++ using-express-server/fakeDB.js
@@ -0,0 +1,27 @@
+const data={
+    user:[
+         {
+        name: 'user1',
+        age: 21,
+        gender: 'male',
+        city: 'India'
+        },{
+            name: 'user2',
+            age: 21,
+            gender: 'male',
+            city: 'India'
+            },{
+                name: 'nikhil',
+                age: 21,
+                gender: 'male',
+                city: 'India'
+                },{
+                    name: 'nikhil',
+                    age: 21,
+                    gender: 'male',
+                    city: 'India'
+                 }
+             ]
+        }
+
+module.exports=data;
